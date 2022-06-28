6fc8a7c83dff4683b7edd391d69cc331f3d910d2
Alexander Tsyganok <stit.2003.1@gmail.com>
2022-06-28 19:08:11 +0300

    Add possibility to get date and time in client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,58 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            const int PORT = 8008;
+            const string IP_ADDR = "127.0.0.1";
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse(IP_ADDR), PORT);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Task readTask = new Task(() =>
+            {
+
+                StringBuilder sb = new StringBuilder();
+                do
+                {
+                    if (socket.Available > 0)
+                    {
+                        int byteCount = 0;
+                        byte[] buffer = new byte[256];
+                        do
+                        {
+                            byteCount = socket.Receive(buffer);
+                            sb.Append(Encoding.Unicode.GetString(buffer, 0, byteCount));
+                        } while (socket.Available > 0);
+                        Console.WriteLine(sb.ToString());
+                        sb.Clear();
+                    }
+                } while (true);
+
+            });
+
+            try
+            {
+                readTask.Start();
+                string msg = String.Empty;
+                socket.Connect(iPEnd);
+                do
+                {
+                    msg = String.Empty;
+                    msg = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(msg);
+                    socket.Send(data);
+                    data = new byte[0];
+
+                }
+                while (!msg.Equals("-end"));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,78 @@ namespace Server
 {
     internal class Program
     {
+        static IPEndPoint iPEnd;
+        static Socket socket;
+        static Socket clientSocket;
+        static Func<string, string> GetResult;
         static void Main(string[] args)
         {
+            const int PORT = 8008;
+            iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Task readTask = new Task(() =>
+            {
+                StringBuilder sb = new StringBuilder();
+                GetResult += GetRes;
+                clientSocket = socket.Accept();
+                do
+                {
+                    int byteCount = 0;
+                    byte[] buffer = new byte[256];
+                    do
+                    {
+                        byteCount = clientSocket.Receive(buffer);
+                        sb.Append(Encoding.Unicode.GetString(buffer, 0, byteCount));
+                    } while (clientSocket.Available > 0);
+
+                    Console.WriteLine($"Client msg:\t{sb.ToString()}");
+                    if (sb[0].Equals('-'))
+                    {
+                        byte[] data = Encoding.Unicode.GetBytes(GetResult?.Invoke(sb.ToString()));
+                        clientSocket.Send(data);
+                    }
+                    sb.Clear();
+
+
+
+                } while (!sb.ToString().Equals("-end"));
+            });
+            try
+            {
+                socket.Bind(iPEnd);
+                socket.Listen(10);
+                readTask.Start();
+                readTask.Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        static string GetRes(string command)
+        {
+            switch (command)
+            {
+                case "-date":
+                    return DateTime.Now.ToShortDateString();
+                case "-time":
+                    return DateTime.Now.ToShortTimeString();
+                case "-help":
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("-date \t displays current date");
+                        sb.AppendLine("-time \t displays current time");
+                        sb.AppendLine("-end \t close programm");
+                        return sb.ToString();
+                    }
+                default:
+                    return "Unknown command";
+            }
         }
     }
 }
